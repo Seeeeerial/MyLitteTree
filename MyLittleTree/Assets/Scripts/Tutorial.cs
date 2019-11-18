da9d6ff838e1e1c2afef8b6cde80da2a28f34c7f
MinJi <wlfl1311@naver.com>
2019-11-18 16:27:52 +0900

    튜토리얼작업중

--- MyLittleTree/Assets/Scripts/Tutorial.cs
+++ MyLittleTree/Assets/Scripts/Tutorial.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
+	public GameObject tree;
+	public GameObject background;
+	public Text talk;
+
+	void Awake()
+	{
+
+	}
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         
